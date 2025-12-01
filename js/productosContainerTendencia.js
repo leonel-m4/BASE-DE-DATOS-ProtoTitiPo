7fc2f8659c6cb1dff3620f0edac73853dc67c060
Leonel <leonelmathiasretamozoxd@gmail.com>
2025-12-01 00:29:29 -0800

    Update productosContainerTendencia.js

--- js/productosContainerTendencia.js
+++ js/productosContainerTendencia.js
@@ -1,55 +1,56 @@
 var contenedor = document.getElementById("productsContainer");
 
+var base = "https://leonel-m4.github.io/BASE-DE-DATOS-ProtoTitiPo";
+
 var productos = [
     {
         nombre: "Polo Neon Wave",
         precio: 15,
-        img: "/img/polos/polo6.jpeg"
+        img: base + "/img/polos/polo6.jpeg"
     },
     {
         nombre: "Pantalón Chino Tapered Fit",
         precio: 135,
-        img: "/img/pantalones/pant7.jpeg"
+        img: base + "/img/pantalones/pant7.jpeg"
     },
     {
         nombre: "Polo Oversize Blanco",
         precio: 90,
-        img: "/img/polos/polo1.jpeg"
+        img: base + "/img/polos/polo1.jpeg"
     },
     {
         nombre: "Pantalón Mom Jeans",
         precio: 120,
-        img: "/img/pantalones/pant1.jpeg"
+        img: base + "/img/pantalones/pant1.jpeg"
     },
-     {
+    {
         nombre: "Polo Urban Black",
         precio: 45,
-        img: "/img/polos/polo2.jpeg"
+        img: base + "/img/polos/polo2.jpeg"
     },
     {
         nombre: "Pantalón Palazzo",
         precio: 70,
-        img: "/img/pantalones/pant2.jpeg"
+        img: base + "/img/pantalones/pant2.jpeg"
     },
-     {
+    {
         nombre: "Pantalón Flare",
         precio: 65,
-        img: "/img/pantalones/pant6.jpeg"
+        img: base + "/img/pantalones/pant6.jpeg"
     },
     {
         nombre: "Pantalón Cargo",
         precio: 110,
-        img: "/img/pantalones/pant4.jpeg"
+        img: base + "/img/pantalones/pant4.jpeg"
     }
 ];
-
 
 contenedor.innerHTML = "";
 
 productos.forEach((p, i) => {
     contenedor.innerHTML += `
         <div class="row">
-            <a href="/products/productoIndex.html?id=${i}">
+            <a href="products/productoIndex.html?id=${i}">
                 <img src="${p.img}" alt="">
             </a>
             <div class="product_text">
