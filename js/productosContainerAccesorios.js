041ee3e4da8df8ef673a057c6b5b5327b8e9f98b
Leonel <leonelmathiasretamozoxd@gmail.com>
2025-12-01 00:21:02 -0800

    Update productosContainerAccesorios.js

--- js/productosContainerAccesorios.js
+++ js/productosContainerAccesorios.js
@@ -1,47 +1,50 @@
 var contenedor = document.getElementById("productsContainer");
 
+var base = "https://leonel-m4.github.io/BASE-DE-DATOS-ProtoTitiPo";
+
 var productos = [
     {
         nombre: "Reloj de Pulsera Clásico",
         precio: 120,
-        img: "https://leonel-m4.github.io/BASE-DE-DATOS-ProtoTitiPo/img/accesorios/accs1.jpeg"
+        img: base + "/img/accesorios/accs1.jpeg"
     },
     {
         nombre: "Bufanda a Cuadros",
         precio: 55,
-        img: "../../img/accesorios/accs2.jpeg"
+        img: base + "/img/accesorios/accs2.jpeg"
     },
     {
         nombre: "Cinturón de Piel con Hebilla Minimalista",
         precio: 65,
-        img: "../../img/accesorios/accs3.jpeg"
+        img: base + "/img/accesorios/accs3.jpeg"
     },
     {
         nombre: "Gafas de Sol Estilo Aviador",
         precio: 95,
-        img: "../../img/accesorios/accs4.jpeg"
+        img: base + "/img/accesorios/accs4.jpeg"
     },
     {
         nombre: "Mochila de Cuero Elegante",
         precio: 140,
-        img: "../../img/accesorios/accs5.jpeg"
+        img: base + "/img/accesorios/accs5.jpeg"
     },
     {
         nombre: "Collar con Dije Geométrico",
         precio: 35,
-        img: "../../img/accesorios/accs6.jpeg"
+        img: base + "/img/accesorios/accs6.jpeg"
     },
     {
         nombre: "Brazaletes Apilables",
         precio: 45,
-        img: "../../img/accesorios/accs7.jpeg"
+        img: base + "/img/accesorios/accs7.jpeg"
     },
     {
         nombre: "Gorro de Lana (Beanie)",
         precio: 30,
-        img: "../../img/accesorios/accs8.jpeg"
+        img: base + "/img/accesorios/accs8.jpeg"
     }
 ];
+
 
 contenedor.innerHTML = "";
 
